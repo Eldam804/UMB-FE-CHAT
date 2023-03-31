c9e1d03de996b1a862c191c13fb73dffeecbc0f3
Eldam804 <adam.kleinedler@gmail.com>
2023-03-31 23:15:51 +0200

    Changed file structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginPageComponent} from "./src/login/login-page/login-page.component";
-import {ChatPageComponent} from "./src/chat/chat-page/chat-page.component";
+import {LoginPageComponent} from "./login/login-page/login-page.component";
+import {ChatPageComponent} from "./chat/chat-page/chat-page.component";
 
 const router: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './src/login/login-page/login-page.component';
-import { LoginFormComponent } from './src/login/login-form/login-form.component';
+import { LoginPageComponent } from './login/login-page/login-page.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormField} from "@angular/material/form-field";
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
@@ -13,7 +13,7 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatListModule} from "@angular/material/list";
 import {MatCardModule} from "@angular/material/card";
-import { ChatPageComponent } from './src/chat/chat-page/chat-page.component';
+import { ChatPageComponent } from './chat/chat-page/chat-page.component';
 import {MatIconModule} from "@angular/material/icon";
 import {RouterOutlet} from "@angular/router";
 
