9dad53723e220a707f096726e2b79892570a3ca7
Tomas306 <124598342+Tomas306@users.noreply.github.com>
2023-04-03 00:50:14 +0200

    add visual private/global chat

---
+++ src/app/user/user-dialog/user-dialog.component.ts
@@ -0,0 +1,10 @@
+import {Component, Inject} from '@angular/core';
+
+@Component({
+  selector: 'app-user-dialog',
+  templateUrl: './user-dialog.component.html',
+  styleUrls: ['./user-dialog.component.css']
+})
+export class test {
+
+}
