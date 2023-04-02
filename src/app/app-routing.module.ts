cbc6bae8fbe2414ed0f20320b5ccc000b60805b1
Eldam804 <adam.kleinedler@gmail.com>
2023-04-02 03:04:29 +0200

    Added /user/user-list component and added it to routing, created a simple list and added multiple users, added some basic css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {LoginPageComponent} from "./login/login-page/login-page.component";
 import {ChatPageComponent} from "./chat/chat-page/chat-page.component";
 import {GlobalChatComponent} from "./chat/global-chat/global-chat.component";
 import {PrivateChatComponent} from "./chat/private-chat/private-chat.component";
+import {UserListComponent} from "./user/user-list/user-list.component";
 
 const router: Routes = [
   {
@@ -17,6 +18,10 @@ const router: Routes = [
   {
     path: "private-chat",
     component: PrivateChatComponent
+  },
+  {
+    path: "user-list",
+    component: UserListComponent
   }
 ]
 
