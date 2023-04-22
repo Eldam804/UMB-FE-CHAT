77dbd06d60710642028353317f7cd1db3dc6f12e
Eldam804 <adam.kleinedler@gmail.com>
2023-04-22 22:59:30 +0200

    Fixed issue with form submitting twice

--- src/app/common/service/auth-header.ts
+++ src/app/common/service/auth-header.ts
@@ -11,12 +11,16 @@ export class AuthHeaderInterceptor implements HttpInterceptor {
     const exclude = 'api/login'
     const exclude1 = 'api/register'
     const exclude2 = 'api/validate/**'
-    if(req.url.search(exclude) === -1 || req.url.search(exclude1) === -1 || req.url.search(exclude2) === -1){
-      const token = this.auth.getToken();
-      if(token !== null){
-        req = req.clone({headers: req.headers.append('Authorization', token)});
-      }
+    const excludeUrls = ['api/login', 'api/register', 'api/validate/**']
+    //if(req.url.search(exclude) === -1){
+    if(excludeUrls.some(url => req.url.includes(url))){
+      return next.handle(req);
     }
+    const token = this.auth.getToken();
+    if(token !== null){
+      req = req.clone({headers: req.headers.append('Authorization', token)});
+    }
+    //}
     return next.handle(req);
   }
 
